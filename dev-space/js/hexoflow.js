926f81354ec0adcbd0f9a4f8af0476c4e5e3bf17
clemb26 <clement.becerra@ig2i.centralelille.fr>
2023-03-12 18:01:16 +0100

    photos equipe

---
+++ dev-space/js/hexoflow.js
@@ -0,0 +1,10 @@
+// ouverture et fermeture du dialogue afficher infos équipes
+function openDialog() {
+    document.getElementById("overlay").style.visibility = "visible";
+    document.getElementById("overlay").style.opacity = "1";
+}
+
+function closeDialog() {
+    document.getElementById("overlay").style.visibility = "hidden";
+    document.getElementById("overlay").style.opacity = "0";
+}
