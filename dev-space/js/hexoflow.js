e804af20b6b76ccbbb218b4400bfb27d82ddd003
clemb26 <clement.becerra@ig2i.centralelille.fr>
2023-03-12 18:36:17 +0100

    modif

--- dev-space/js/hexoflow.js
+++ dev-space/js/hexoflow.js
@@ -8,3 +8,19 @@ function closeDialog() {
     document.getElementById("overlay").style.visibility = "hidden";
     document.getElementById("overlay").style.opacity = "0";
 }
+
+
+// ouvrir un cv au format pdf
+function openCV(personne) {
+    switch (personne) {
+        case 'robin':
+            window.open('application/CV_DUFLOT.pdf', '_blank');
+            break;
+        case 'jeanne':
+            window.open('application/CV_DUFLOT.pdf', '_blank');
+            break;
+        case 'hugo':
+            window.open('application/CV_DUFLOT.pdf', '_blank');
+            break;
+    }
+}
